69b6120db3b962b8c4770c7e9f7e6d09f6bd1943
Marvin V Prakash <marvinprakash@gmail.com>
2025-01-18 20:51:27 +0530

    correct clouflared config

--- config/services/cloudflared.nix
+++ config/services/cloudflared.nix
@@ -3,5 +3,20 @@
 {
   services.cloudflared = {
     enable = true;
+
+    tunnels = {
+      "241dce64-be43-40ed-8b0a-50f7e89096a1" = {
+        credentialsFile = "${config.sops.secrets.cloudflared-creds.path}";
+        ingress = {
+          "*.morphotech.xyz" = {
+            service = "http://localhost:8000";
+            path = "/*.(jpg|png|css|js)";
+          };
+        };
+        default = "http_status:404";
+      };
+    };
+    
+    
   }
 }
