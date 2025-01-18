90ce6b3bb497735087d0064c90fbe953cfaf67b9
Marvin V Prakash <marvinprakash@gmail.com>
2025-01-18 22:28:34 +0530

    better config

--- config/services/cloudflared.nix
+++ config/services/cloudflared.nix
@@ -3,8 +3,6 @@
 {
   services.cloudflared = {
     enable = true;
-    package = pkgs.cloudflared;
-    user= "marvin";    
     tunnels = {
       "6bf978c7-d774-4c8b-97d2-d2b21dc932fc" = {
         credentialsFile = "/home/marvin/.cloudflared/6bf978c7-d774-4c8b-97d2-d2b21dc932fc.json";
@@ -13,7 +11,6 @@
           # Main website configuration
           "morphotech.xyz" = {
             service = "http://localhost:3000";
-            path = "/*.(jpg|png|css|js)";
           };
           # The default case needs to be a separate key in ingress
         };
